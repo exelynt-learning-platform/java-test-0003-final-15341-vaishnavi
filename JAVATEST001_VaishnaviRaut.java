55096449e075f62ab7ee3a05bdb9b0f6635aa262
vaishuraut841 <154678214+vaishuraut841@users.noreply.github.com>
2026-03-14 15:59:09 +0530

    Add files via upload

---
+++ JAVATEST001_VaishnaviRaut.java
@@ -0,0 +1,34 @@
+public class JAVATEST001_VaishnaviRaut{
+public static void main(String[] args){
+    int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+            int num = i % 2;  // starting value for each row
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = (num == 0) ? 1 : 0; // toggle between 0 and 1
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
