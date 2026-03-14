76596e603534dda6bc62a79a70b5e70f242deec6
vaishuraut841 <154678214+vaishuraut841@users.noreply.github.com>
2026-03-14 16:00:09 +0530

    Update JAVATEST001_VaishnaviRaut.java

--- JAVATEST001_VaishnaviRaut.java
+++ JAVATEST001_VaishnaviRaut.java
@@ -4,11 +4,11 @@ public static void main(String[] args){
 
         for (int i = 1; i <= n; i++) {
 
-            int num = i % 2;  // starting value for each row
+            int num = i % 2;  
 
             for (int j = 1; j <= i; j++) {
                 System.out.print(num + " ");
-                num = (num == 0) ? 1 : 0; // toggle between 0 and 1
+                num = (num == 0) ? 1 : 0; 
             }
 
             System.out.println();
